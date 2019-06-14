b7d632c06f5bea256c52fc4de00539fe43784493
ThangScout <thangvatvo1102@gmail.com>
2019-06-14 11:53:11 +0700

    Add utilities folder and add calculator.js file

---
+++ utilities/calculator.js
@@ -0,0 +1,26 @@
+class Calcu {
+    constructor(calcu, numberA, numberB){
+        this.calcu = calcu;
+        this.numberA = numberA;
+        this.numberB = numberB;
+    }
+
+    _getStringCal(){
+        const { calcu, numberA, numberB} = this;
+        if(calcu ==='SUM')
+            return `${numberA} + ${numberB}`;
+        if(calcu ==='SUB')
+            return `${numberA} - ${numberB}`;
+        if(calcu ==='MUL')
+            return `${numberA} * ${numberB}`;
+        return `${numberA} / ${numberB}`;
+    }
+
+    _getResultCal(){
+        const calcuString = this._getStringCal();
+        const result = eval(calcuString);
+        return `${calcuString} = ${result}`
+    }
+}
+
+module.exports = Calcu;
